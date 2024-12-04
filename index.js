5f97ac4c17bcb202621b23986b380c7134ac1f88
fabrice-vallee <fabrice.vallee@3wa.io>
2024-12-04 09:00:01 +0000

    My first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+
+const STUDENTS_FILES_PATH = 'students';
+
+const studentsFiles = [
+];
+
+if (studentsFiles.length === 0) {
+    console.log('No student file provided');
+    process.exit(0);
+}
+
+for (let i = 0 ; i < studentsFiles.length ; i++) {
+    const fileName = studentsFiles[i];
+    
+    try {
+        const fileContent = fs.readFileSync(
+            path.join(STUDENTS_FILES_PATH, fileName),
+            'utf-8'
+        );
+
+        console.log(`---- Student file ${fileName} content ----`);
+        console.log(fileContent);
+
+    } catch (err) {
+        console.error(`Error when reading file ${fileName}`);
+        console.error(err);
+    }
+}
