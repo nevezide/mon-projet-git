c28b35854e558b72b89579205f098a36c73c15a1
Clem-Coding <clementine.blondeau@gmail.com>
2024-12-05 11:01:06 +0100

    feat(Students) Add my student gile

--- index.js
+++ index.js
@@ -1,30 +1,28 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const STUDENTS_FILES_PATH = 'students';
+const STUDENTS_FILES_PATH = "students";
 
-const studentsFiles = [
-];
+const studentsFiles = ["Clementine"];
 
 if (studentsFiles.length === 0) {
-    console.log('No student file provided');
-    process.exit(0);
+  console.log("No student file provided");
+  process.exit(0);
 }
 
-for (let i = 0 ; i < studentsFiles.length ; i++) {
-    const fileName = studentsFiles[i];
-    
-    try {
-        const fileContent = fs.readFileSync(
-            path.join(STUDENTS_FILES_PATH, fileName),
-            'utf-8'
-        );
+for (let i = 0; i < studentsFiles.length; i++) {
+  const fileName = studentsFiles[i];
 
-        console.log(`---- Student file ${fileName} content ----`);
-        console.log(fileContent);
+  try {
+    const fileContent = fs.readFileSync(
+      path.join(STUDENTS_FILES_PATH, fileName),
+      "utf-8"
+    );
 
-    } catch (err) {
-        console.error(`Error when reading file ${fileName}`);
-        console.error(err);
-    }
+    console.log(`---- Student file ${fileName} content ----`);
+    console.log(fileContent);
+  } catch (err) {
+    console.error(`Error when reading file ${fileName}`);
+    console.error(err);
+  }
 }
