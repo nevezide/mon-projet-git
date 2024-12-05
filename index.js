4f29698cf5d8fe97912b0230f96d9346d4e2cfd4
Thomas Riou <thomasriou35@gmail.com>
2024-12-05 12:39:41 +0000

    refactor(index.js) MaJ tableau

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
-const STUDENTS_FILES_PATH = "students";
+const STUDENTS_FILES_PATH = 'students';
 
-const studentsFiles = [
+const studentsFiles = const studentsFiles = [
   "Thomas",
   "Tristan.txt",
   "Tehash",
@@ -21,23 +21,25 @@ const studentsFiles = [
 ];
 
 if (studentsFiles.length === 0) {
-  console.log("No student file provided");
-  process.exit(0);
+    console.log('No student file provided');
+    process.exit(0);
 }
 
-for (let i = 0; i < studentsFiles.length; i++) {
-  const fileName = studentsFiles[i];
+for (let i = 0 ; i < studentsFiles.length ; i++) {
+    const fileName = studentsFiles[i];
 
-  try {
-    const fileContent = fs.readFileSync(
-      path.join(STUDENTS_FILES_PATH, fileName),
-      "utf-8"
-    );
+    
+    try {
+        const fileContent = fs.readFileSync(
+            path.join(STUDENTS_FILES_PATH, fileName),
+            'utf-8'
+        );
+
+        console.log(`---- Student file ${fileName} content ----`);
+        console.log(fileContent);
 
     } catch (err) {
         console.error(`Error when reading file ${fileName}`);
         console.error(err);
     }
 }
-console.log(`---- Student file ${fileName} content ----`);
-console.log(fileContent);
